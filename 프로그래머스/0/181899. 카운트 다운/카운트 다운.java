8bce27f9214b444eeddb6cadef9a5704c79dbcd0
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-03-08 22:37:11 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start - end_num + 1];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = start--;
+        }
+        return answer;
+    }
+}
