9c52792b31ab90babc35457d5a76561a5ce4c064
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-03-10 16:50:12 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n % Math.sqrt(n) == 0) {
+            answer = 1;
+        }
+        else {
+            answer = 2;
+        }
+        return answer;
+    }
+}
