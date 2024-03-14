71b8b1fed9f72f0714aaa1006c8e50c920297c21
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-03-14 22:37:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int price) {
+    int answer = 0;
+    if(price >= 500000) {
+        price *= 0.8;
+    }
+    else if(price >= 300000) {
+        price *= 0.9;
+    }
+    else if(price >= 100000) {
+        price *= 0.95;
+    }
+    return price;
+    }
+}
