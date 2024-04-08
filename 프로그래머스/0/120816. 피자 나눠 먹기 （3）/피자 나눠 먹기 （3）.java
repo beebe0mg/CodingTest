598d93b5a9667fdd35c0738da17447bf053515be
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-04-09 00:14:33 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        if(n%slice!=0) answer = n/slice+1;
+        else answer = n/slice;
+        return answer;
+    }
+}
