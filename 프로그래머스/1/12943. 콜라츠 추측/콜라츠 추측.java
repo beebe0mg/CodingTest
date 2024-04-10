ab3655eaf51d1c0925cc099f94e8e3c25cb24a52
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-04-11 00:04:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        while(num!=1) {
+            num = (num%2==0) ? num/2 : num*3+1;
+            answer++;
+            if(answer>=500) {
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
