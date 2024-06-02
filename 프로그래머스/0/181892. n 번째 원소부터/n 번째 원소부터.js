df2fbed5cba41e9690893614c509e9c16cbf8044
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-06-02 23:16:18 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -1,7 +1,8 @@
 function solution(num_list, n) {
     var answer = [];
-    for(let i=n-1; i<num_list.length; i++) {
-        answer.push(num_list[i]);
-    }
+    // for(let i=n-1; i<num_list.length; i++) {
+    //     answer.push(num_list[i]);
+    // }
+    answer = num_list.slice(n-1);
     return answer;
 }
