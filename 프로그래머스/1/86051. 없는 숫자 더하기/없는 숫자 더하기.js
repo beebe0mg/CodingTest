ada450b9ed5223fcc77a0900fd57556bbcd76221
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-06-25 10:07:52 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    var answer = 0;
+    for(var i=0; i<=9; i++) {
+        if(!numbers.includes(i)) {
+            answer += i;
+        } 
+    }
+    return answer;
+}
