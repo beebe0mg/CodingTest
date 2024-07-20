17b70463bb8a10ada60dcd259cfab1e00a12980b
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-07-20 14:02:54 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -3,14 +3,10 @@ function solution(s) {
     s = s.split(" ");
     for(let i=0; i<s.length; i++) {
         for(let j=0; j<s[i].length; j++) {
-            if(j%2===0) {
-                answer += s[i][j].toUpperCase()
-            }
-            else answer += s[i][j].toLowerCase()
+            if(j % 2 === 0) answer += s[i][j].toUpperCase();
+            else answer += s[i][j].toLowerCase();
         }
-        if(i < s.length - 1)
-        answer += " "
+        if(i !== s.length - 1) answer += " ";
     }
-    
     return answer;
 }
