914884b67f10aeccebcd1da0450e0e4a6f4901c1
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-07-26 09:29:55 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    var answer = '';
+    s = s.split("");
+    s.sort(function compare(a, b) {
+        if (a > b) return -1;
+        if (a < b) return 1;
+        return 0;
+    });
+    answer = s.join("");
+    return answer;
+}
