57b4230a30c7fe47df5f3ebe9e4b53d8d23c325b
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-12-15 11:07:35 +0900

    [level 1] Title: 행렬의 덧셈, Time: 6.24 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,9 @@
+function solution(arr1, arr2) {
+    var answer = arr1;
+    for(let i=0; i<arr1.length; i++) {
+        for(let j=0; j<arr1[i].length; j++) {
+            answer[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+    return answer;
+}
