95bca2839e7b9eab9d882ade2059ccfcf36869a6
정상미 <128356174+beebe0mg@users.noreply.github.com>
2024-12-22 23:06:07 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,3 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS
+    WHERE NAME = 'Lucy' OR NAME = 'Ella' OR NAME = 'Pickle' or NAME = 'Rogan' or NAME = 'Sabrina' or NAME = 'Mitty'
