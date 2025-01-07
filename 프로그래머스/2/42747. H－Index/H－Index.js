b3264f25b045475c1f438b1f311df8443d2a5046
정상미 <128356174+beebe0mg@users.noreply.github.com>
2025-01-07 16:03:56 +0900

    [level 2] Title: H-Index, Time: 14.62 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,15 @@
+function solution(citations) {
+    var answer = 0;
+    
+    citations.sort((a,b) => b-a)
+    console.log(citations)
+    
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] >= i + 1) {
+            answer = i + 1;
+        } else {
+            break;
+        }
+    }
+    return answer;
+}
