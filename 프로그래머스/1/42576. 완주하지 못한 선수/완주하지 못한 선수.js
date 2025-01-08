f16ee51a90d8eaa36c234a37930ad3a5494bd2cf
정상미 <128356174+beebe0mg@users.noreply.github.com>
2025-01-08 12:55:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 114.18 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -5,8 +5,5 @@ function solution(participant, completion) {
     for(let i=0; i<participant.length; i++) {
         if(participant[i] !== completion[i]) return participant[i]
     }
-    for(let j=0; j<participant.length; j++) {
-        if(!completion.includes(participant[j])) answer = participant[j]
-    }
     return answer;
 }
